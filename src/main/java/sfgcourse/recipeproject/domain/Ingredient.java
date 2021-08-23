9426ff186f495510c5c91f18be4c4147bbed2fe7
ToJoos <joosx2000@gmail.com>
2021-08-23 14:49:09 +0200

    135,136. Adding Unit of measure and relation ship between Ingredient and UOM (only one way ING -> UOM)

--- src/main/java/sfgcourse/recipeproject/domain/Ingredient.java
+++ src/main/java/sfgcourse/recipeproject/domain/Ingredient.java
@@ -11,10 +11,12 @@ public class Ingredient {
 
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER) //fetch type not needed because it's default behavior
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -46,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
---
+++ src/main/java/sfgcourse/recipeproject/domain/UnitOfMeasure.java
@@ -0,0 +1,12 @@
+package sfgcourse.recipeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+}

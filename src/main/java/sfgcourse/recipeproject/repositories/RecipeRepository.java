ad714f498cd7356b424dccb7c860b0be63dd01d1
ToJoos <joosx2000@gmail.com>
2021-08-24 12:32:30 +0200

    139. Adding data repositories.

---
+++ src/main/java/sfgcourse/recipeproject/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package sfgcourse.recipeproject.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sfgcourse.recipeproject.domain.Recipe;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
---
+++ src/main/java/sfgcourse/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package sfgcourse.recipeproject.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sfgcourse.recipeproject.domain.UnitOfMeasure;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}

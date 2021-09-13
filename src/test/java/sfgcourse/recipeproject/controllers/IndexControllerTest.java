b6b513ac8ac69197a79d5ea9cfed4536145b1192
ToJoos <joosx2000@gmail.com>
2021-09-13 11:44:30 +0200

    177. Argument capture

--- src/test/java/sfgcourse/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/sfgcourse/recipeproject/controllers/IndexControllerTest.java
@@ -2,15 +2,20 @@ package sfgcourse.recipeproject.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import sfgcourse.recipeproject.domain.Notes;
+import sfgcourse.recipeproject.domain.Recipe;
 import sfgcourse.recipeproject.services.RecipeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
     IndexController indexController;
@@ -30,8 +35,26 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
-        assertEquals("index",indexController.getIndexPage(model));
-        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipeSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+        //then
+        assertEquals("index", viewName);
+        verify(model,times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         verify(recipeService,times(1)).getRecipes();
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }

25c5292bc0f5bf7452282a59f288afe4d9eb0db8
ToJoos <joosx2000@gmail.com>
2021-09-25 15:44:53 +0200

    Added unit tests for findRecipeCommandById and recipeForm: postNew/Update

--- src/test/java/sfgcourse/recipeproject/services/RecipeServiceImplTest.java
+++ src/test/java/sfgcourse/recipeproject/services/RecipeServiceImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import sfgcourse.recipeproject.commands.RecipeCommand;
 import sfgcourse.recipeproject.converters.RecipeCommandToRecipe;
 import sfgcourse.recipeproject.converters.RecipeToRecipeCommand;
 import sfgcourse.recipeproject.domain.Recipe;
@@ -50,7 +51,7 @@ class RecipeServiceImplTest {
     }
 
     @Test
-    void getRecipesById() {
+    void findRecipeById() {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
@@ -61,6 +62,26 @@ class RecipeServiceImplTest {
 
         assertNotNull(recipeReturned);
 
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    void findRecipeCommandById() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> optionalRecipe = Optional.of(recipe);
+
+        when(recipeRepository.findById(any())).thenReturn(optionalRecipe);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommandConverter.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandFound = recipeService.findCommandById(1L);
+
+        assertNotNull(commandFound);
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, never()).findAll();
     }

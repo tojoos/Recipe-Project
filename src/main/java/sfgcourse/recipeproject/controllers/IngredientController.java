3c68f2f7977a11028cd0a6b2ab0c04468bf52260
ToJoos <joosx2000@gmail.com>
2021-09-27 22:41:54 +0200

    Small mapping corrections.

--- src/main/java/sfgcourse/recipeproject/controllers/IngredientController.java
+++ src/main/java/sfgcourse/recipeproject/controllers/IngredientController.java
@@ -2,8 +2,8 @@ package sfgcourse.recipeproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import sfgcourse.recipeproject.services.RecipeService;
 
 @Controller
@@ -15,7 +15,7 @@ public class IngredientController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/{id}/ingredients")
+    @GetMapping("/recipe/{id}/ingredients")
     public String showIngredients(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
         return "recipe/ingredient/list";

1ecb74fb6c0964f25bb1725ca7352d4c44786434
ToJoos <joosx2000@gmail.com>
2021-10-15 13:01:57 +0200

    Added extra information for error page.

--- src/main/java/sfgcourse/recipeproject/controllers/RecipeController.java
+++ src/main/java/sfgcourse/recipeproject/controllers/RecipeController.java
@@ -54,10 +54,13 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
+    public ModelAndView handleNotFound(Exception exception) {
         log.error("handle not found exception");
+        log.error(exception.getMessage());
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("errorPage");
+        modelAndView.addObject("exception", exception);
+
         return modelAndView;
     }
 }
